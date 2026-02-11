a460916aca5d2e024a7fc058f57bd86d3543b69e
mohamaddaakour <mohamaddaakour8@gmail.com>
2026-02-11 17:10:53 +0000

    working on module-6

---
+++ module-6/alchemy/elements.py
@@ -0,0 +1,11 @@
+def create_fire():
+    return "Fire element created"
+
+def create_water():
+    return "Water element created"
+
+def create_eartch():
+    return "Earth element created"
+
+def create_air():
+    return "Air element created"
---
+++ module-6/ft_sacred_scroll.py
@@ -0,0 +1,9 @@
+import alchemy.elements
+
+def main():
+    print("\n===Sacred Scroll Mastery ===\n")
+
+    print("Testing direct module access:")
+    print(f"alchemy.elements.create_fire(): {alchemy.elements.create_fire()}")
+
+main()
